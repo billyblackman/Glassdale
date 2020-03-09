121097f810a91c53598369d2a836de73dfbbe01c
William Daniel Blackman <blackmanbilly@gmail.com>
2020-03-09 11:15:22 -0500

    updated js file structure for criminal database to match officer database

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// main.js
+import { getCriminals } from './criminals/CriminalProvider.js'
+
+getCriminals().then(
+    /*
+        Now that you have the data, what
+        component should be rendered?
+    */
+)
