ec9b279916560d470d7dfe5b5216e227a2b8c539
William Daniel Blackman <blackmanbilly@gmail.com>
2020-03-11 13:47:25 -0500

    created custom event to de-couple criminal list and conviction select and filter criminals by conviction on home page

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -7,6 +7,27 @@ import { useConvictions } from "./ConvictionProvider.js"
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".filters__crime")
 
+// Get a reference to DOM element where event will happen and be heard
+const eventHub = document.querySelector(".container")
+
+// Add event listener and dispatch custom event
+contentTarget.addEventListener("change", changeEvent => {
+    if (changeEvent.target.id === "crimeSelect") {
+        const theCrimeThatWasChosen = changeEvent.target.value
+
+        const crimeChosenEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                chosenCrime: theCrimeThatWasChosen
+            }
+        })
+
+// Dispatch the custom event
+        eventHub.dispatchEvent(crimeChosenEvent)
+    }
+})
+
+
+
 const ConvictionSelect = () => {
     // Get all convictions from application state
     const convictions = useConvictions()
